f0a7a98d81fc95bdc18a3bf5721d6a2472414bfd
DevHack136 <trystan.legrand-simon@epitech.eu>
2023-10-06 14:06:14 +0200

    Mise à jour

--- Job02/myupdate.sh
+++ Job02/myupdate.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# Met à jour la liste des paquets disponibles et effectue une mise à jour des paquets
+# L'option "-y" est utilisée pour répondre automatiquement "oui" aux demandes de confirmation
 sudo apt update && sudo apt upgrade -y

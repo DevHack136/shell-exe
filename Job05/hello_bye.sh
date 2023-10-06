56961385f904324324614a92b13cec215d2eadb9
DevHack136 <trystan.legrand-simon@epitech.eu>
2023-10-06 14:02:10 +0200

    Mise à jour

--- Job05/hello_bye.sh
+++ Job05/hello_bye.sh
@@ -2,12 +2,18 @@
 
 # Hello
 if [ "$1" == "Hello" ]; then
+    # Vérifie si le premier argument est égal à "Hello".
     echo "Bonjour, je suis un script !"
+    # Si l'argument est "Hello", affiche "Bonjour, je suis un script !"
 
 # Bye
 elif [ "$1" == "Bye" ]; then
+    # Si l'argument n'est pas "Hello" mais égal à "Bye".
     echo "Au revoir et bonne journée !"
+    # Si l'argument est "Bye", affiche "Au revoir et bonne journée !"
 
 else
+    # Si l'argument n'est ni "Hello" ni "Bye".
     echo "Pas d'argument proposé ou mauvais argument !"
+    # Affiche "Pas d'argument proposé ou mauvais argument !"
 fi

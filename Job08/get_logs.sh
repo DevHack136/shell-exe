51f1419883a7bedf2dd1293f1bb79799e84fd5d7
DevHack136 <trystan.legrand-simon@epitech.eu>
2023-10-06 14:53:51 +0200

    Mise à jour

--- Job08/get_logs.sh
+++ Job08/get_logs.sh
@@ -4,7 +4,7 @@
 Ctime=$(date +"%d-%m-%Y-%H:%M")
 
 # Enregistre le nombre de connexions dans un fichier nommé "number_connection-$Ctime".
-echo $(cat /var/log/auth.log | grep "session opened for user $1" | wc -l) > "number_connection-$Ctime"
+echo $(last $1 | grep "$1" | wc -l) > "number_connection-$Ctime"
 
 # Crée une archive TAR avec l'option "-c" pour créer une nouvelle archive et "-f" pour spécifier le nom de l'archive.
 # Le nom de l'archive est basé sur la variable Ctime et est stocké dans le répertoire "Backup".
